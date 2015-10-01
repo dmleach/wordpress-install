bc0b3a67cb46af1b97de9dc4dd2ab0ab1453c1eb
Dave Leach <dave@dmleach.com>
2015-10-01 16:45:37 -0400

    Added WordPress configuration, including wp-config and redirect

--- install.sh
+++ install.sh
@@ -12,6 +12,15 @@ function print () {
     fi
 }
 
+function installpackage() {
+    if ! [ -z $1 ]
+    then
+        if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ]
+        then apt-get install $1
+        fi
+    fi
+}
+
 print "WordPress installation" "bold"
 print ""
 
@@ -27,10 +36,16 @@ print "(e.g. to install to /files/wp/"$site", enter '/files/wp')"
 read rootdir
 print ""
 
+##### PACKAGE INSTALLATION #####
+print "Installing needed packages" "underline"
+print ""
+installpackage "php5-gd"
+installpackage "libssh2-php"
+
 ##### MYSQL DATABASE CREATION #####
 # Initialize variables
 sitemysqldb=$site"wpdb"
-sitemysqlpassword=$site"pw"
+sitemysqlpassword="$(openssl rand -base64 10)"
 sitemysqlscript=$site".sql"
 sitemysqluser=$site"user"
 
@@ -40,7 +55,6 @@ print ""
 
 # Create a script file with the MySql commands
 echo "CREATE DATABASE IF NOT EXISTS "$sitemysqldb";" > $sitemysqlscript
-#echo "CREATE USER IF NOT EXISTS "$sitemysqluser"@localhost IDENTIFIED BY '"$sitemysqlpassword"';" >> $sitemysqlscript
 echo "GRANT ALL PRIVILEGES ON "$sitemysqldb".* TO "$sitemysqluser"@localhost IDENTIFIED BY '"$sitemysqlpassword"';" >> $sitemysqlscript
 echo "FLUSH PRIVILEGES;" >> $sitemysqlscript
 
@@ -51,10 +65,13 @@ trap "rm "$sitemysqlscript EXIT
 # Run the script file using the MySql command line utility
 mysql -u root < $sitemysqlscript
 
-##### WORDPRESS DOWNLOAD *****
+##### WORDPRESS DOWNLOAD #####
 print "WordPress download" "underline"
 print "Downloading latest WordPress installation"
 print ""
+
+# Initialize variables
+wpsitedir=$rootdir"/"$site
 
 # Download the file and create a link to the local version. The link and
 # -N option prevents downloading the file again if the remote is no newer
@@ -62,8 +79,47 @@ wpzipname="latest.tar.gz"
 wget -qN http://wordpress.org/$wpzipname
 
 # Extract the file to the requested directory
-if ! [ -d $rootdir"/"$site ]
-then mkdir $rootdir"/"$site
+if ! [ -d $wpsitedir ]
+then mkdir $wpsitedir
 fi
 
-tar xzf latest.tar.gz --directory $rootdir"/"$site --strip-components=1
+tar xzf latest.tar.gz --directory $wpsitedir --strip-components=1
+
+##### WORDPRESS CONFIGURATION #####
+print "WordPress configuration" "underline"
+print "Creating configuration files"
+print ""
+
+# Initialize variables
+wpconfigdir=$wpsitedir".config"
+wpconfigpath=$wpconfigdir"/"wp-config.php
+wpconfigredirect=$wpsitedir"/"wp-config.php
+
+# Create the directory that will hold the config file
+if ! [ -d $wpconfigdir ]
+then mkdir $wpconfigdir
+fi
+
+# Create the wp-config file with values for the site
+echo "<?php" > $wpconfigpath
+echo "" >> $wpconfigpath
+echo "define('DB_NAME', '"$sitemysqldb"');" >> $wpconfigpath
+echo "define('DB_USER', '"$sitemysqluser"');" >> $wpconfigpath
+echo "define('DB_PASSWORD', '"$sitemysqlpassword"');" >> $wpconfigpath
+echo "define('DB_HOST', 'localhost');" >> $wpconfigpath
+echo "define('DB_CHARSET', 'utf8mb4');" >> $wpconfigpath
+echo "define('DB_COLLATE', 'utf8mb4_general_ci');" >> $wpconfigpath
+echo "" >> $wpconfigpath
+curl -s 1 https://api.wordpress.org/secret-key/1.1/salt >> $wpconfigpath
+echo "" >> $wpconfigpath
+echo "$table_prefix  = 'tbl_';" >> $wpconfigpath
+echo "define('WP_DEBUG', true);" >> $wpconfigpath
+
+# Create a wp-config file in the site's directory that redirects to the
+# real one in the config directory
+echo "<?php" > $wpconfigredirect
+echo "/** Absolute path to the WordPress directory. */" >> $wpconfigredirect
+echo "if ( !defined('ABSPATH') )" >> $wpconfigredirect
+echo "    define('ABSPATH', dirname(__FILE__) . '/');" >> $wpconfigredirect
+echo "/** Location of your WordPress configuration. */" >> $wpconfigredirect
+echo "require_once(ABSPATH . '../"$site".config/wp-config.php');" >> $wpconfigredirect
